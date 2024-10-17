ef5b68d874e57fb4d9a61f601ec34ba3ef33a58b
saksham-guptaa <creator.sakshamgupta@gmail.com>
2024-10-17 13:43:28 +0530

    text center

--- src/pages/WindowPage.jsx
+++ src/pages/WindowPage.jsx
@@ -32,88 +32,95 @@ const WindowPage = () => {
 
       {/* Main content layout */}
       <div className="flex min-h-screen">
-        {/* Folders section */}
-        <div className="flex flex-col min-h-screen justify-between py-10 ml-4">
-          {/* Dexscreener Folder */}
-          <div className="text-center">
-            <img
-              src="/folder_image.png"
-              className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-              alt="Dexscreener"
-            />
-            <p className="text-[8px] md:text-sm">Dexscreener</p>
-          </div>
+  {/* Folders section */}
+  <div className="flex flex-col min-h-screen justify-between py-10 ml-4">
+    {/* Dexscreener Folder */}
+    <Link href="https://dexscreener.com/ethereum/0xd56ce7a608a2bc865dfab82ec22c02ec1c757ac0?__cf_chl_rt_tk=9d8SklMDbHuaQeGH3rdx_IeSuJkubGggA7OahmZ_yx4-1729101657-1.0.1.1-DTGllecUHjXZcxn35.Oek6sXAXdmyGp7cVyBGJtAiIw" target='_blank' >
+    <div className="flex flex-col items-center text-center">
+      <img
+        src="/folder_image.png"
+        className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+        alt="Dexscreener"
+      />
+      <p className="mt-2 text-[8px] md:text-sm">Dexscreener</p>
+    </div>
+    </Link>
 
-          {/* Know Your Meme and About Folders */}
-          <div className="flex space-x-5">
-            <div className="text-center">
-              <img
-                src="/folder_image.png"
-                className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-                alt="Know Your Meme"
-              />
-              <p className="mt-2 w-20 text-[8px] md:text-sm mx-auto">Know Your Meme</p>
-            </div>
-          </div>
-
-          {/* Telegram and X Folders */}
-          <div className="flex space-x-20">
-            <div className="text-center">
-              <img
-                src="/folder_image.png"
-                className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-                alt="Telegram"
-              />
-              <p className="mt-2 text-[8px] md:text-sm">Telegram</p>
-            </div>
-          </div>
-        </div>
-
-        {/* Bottom Right Error Link */}
-        <Link href="/ErrorPage">
-          <div className="absolute bottom-[20%] right-[2%]">
-            <img
-              src="/folder_image.png"
-              className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-              alt="DO NOT OPEN"
-            />
-            <p className="mt-2 text-[8px] md:text-sm">DO NOT OPEN</p>
-          </div>
-        </Link>
-
-        {/* Memes Folder */}
-        <div className="absolute bottom-[2%] right-[15%] text-center">
-          <img
-            src="/folder_image.png"
-            className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-            alt="Memes"
-          />
-          <p className="mt-2 text-[8px] md:text-sm">Memes</p>
-        </div>
-
-        <div className="flex max-h-screen flex-col-reverse space-y-14 mb-5 space-y-reverse">
-          <div className="text-center">
-            <img
-              src="/folder_image.png"
-              className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-              alt="X"
-            />
-            <p className="mt-2 text-[8px] md:text-sm">X</p>
-          </div>
-
-          <Link href="/AboutPage">
-            <div className="text-center">
-              <img
-                src="/folder_image.png"
-                className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24 mx-auto"
-                alt="About"
-              />
-              <p className="mt-2 text-[8px] md:text-sm">About</p>
-            </div>
-          </Link>
-        </div>
+    {/* Know Your Meme and About Folders */}
+    <div className="flex space-x-5">
+      <Link href="https://knowyourmeme.com/memes/smug-frog/photos" target='_blank' ></Link>
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/folder_image.png"
+          className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+          alt="Know Your Meme"
+        />
+        <p className="mt-2 w-20 text-[8px] md:text-sm">Know Your Meme</p>
       </div>
     </div>
+
+    {/* Telegram and X Folders */}
+    <div className="flex space-x-20">
+      <Link href="https://t.me/SmugCTOPortal" target='_blank'>
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/folder_image.png"
+          className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+          alt="Telegram"
+        />
+        <p className="mt-2 text-[8px] md:text-sm">Telegram</p>
+      </div>
+      </Link>
+    </div>
+  </div>
+
+  {/* Bottom Right Error Link */}
+  <Link href="/ErrorPage">
+    <div className="absolute bottom-[20%] right-[2%] flex flex-col items-center text-center">
+      <img
+        src="/folder_image.png"
+        className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+        alt="DO NOT OPEN"
+      />
+      <p className="mt-2 text-[8px] md:text-sm">DO NOT OPEN</p>
+    </div>
+  </Link>
+
+  {/* Memes Folder */}
+  <div className="absolute bottom-[2%] right-[15%] flex flex-col items-center text-center">
+    <img
+      src="/folder_image.png"
+      className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+      alt="Memes"
+    />
+    <p className="mt-2 text-[8px] md:text-sm">Memes</p>
+  </div>
+
+  <div className="flex max-h-screen flex-col-reverse space-y-14 mb-5 space-y-reverse">
+    <Link href="https://x.com/smugthepepe" target='_blank' >
+    <div className="flex flex-col items-center text-center">
+      <img
+        src="/folder_image.png"
+        className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+        alt="X"
+      />
+      <p className="mt-2 text-[8px] md:text-sm">X</p>
+    </div>
+    </Link>
+
+    <Link href="/AboutPage">
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/folder_image.png"
+          className="h-8 w-10 md:h-16 md:w-20 lg:h-20 lg:w-24"
+          alt="About"
+        />
+        <p className="mt-2 text-[8px] md:text-sm">About</p>
+      </div>
+    </Link>
+  </div>
+</div>
+</div>
   );
 };
 
